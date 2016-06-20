b48332486e3db4815d273898601dcde3727b0d0d
khai96_ <hvksmr1996@gmail.com>
2016-06-20 19:21:50 +0700

    remove unnecessary g++ flags

--- env-config.sh
+++ env-config.sh
@@ -3,5 +3,4 @@ echo -o ./main.exe ./main.cpp \
     -D FREEGLUT_STATIC \
     -I/c/freeglut/include \
     -L/c/freeglut/lib/x64 \
-    -lfreeglut_static -lopengl32 -lwinmm -lgdi32 \
-    -Wl,--subsystem,windows
+    -lfreeglut_static -lopengl32 -lwinmm -lgdi32 
